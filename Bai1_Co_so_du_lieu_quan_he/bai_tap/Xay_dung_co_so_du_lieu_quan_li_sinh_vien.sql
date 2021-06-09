3c1b1c0d8ddb3ffcd676eb34577b23d801fb9937
TechCare <you@example.com>
2021-06-09 16:55:06 +0700

    commit

---
+++ Bai1_Co_so_du_lieu_quan_he/bai_tap/Xay_dung_co_so_du_lieu_quan_li_sinh_vien.sql
@@ -0,0 +1,11 @@
+create database student_management;
+create table Class(
+ID int,
+Name varchar(45)
+);
+create table Teacher(
+id int,
+Name varchar(45),
+Age int,
+Country varchar(45)
+);
---
+++ Bai1_Co_so_du_lieu_quan_he/thuc_hanh/Tao_bang_tren_MySQL.sql
@@ -0,0 +1,7 @@
+create database student_management;
+create table student(
+id int,
+name varchar(45),
+age int,
+country varchar(45)
+);
